49b4ce09be26c333157144ed387418e75e07d0cd
eliud campos hernandez <eliudjosuec@gmail.com>
2022-05-21 11:46:27 -0300

    redux 2105

--- src/Redux/componentes/Pokemones.jsx
+++ src/Redux/componentes/Pokemones.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { obtenerPokemonesAccion } from '../pokeDucks'
+import { anteriorPokemonAction, obtenerPokemonesAccion, siguientePokemonAction } from '../pokeDucks'
 
 const Pokemones = () => {
     const dispatch = useDispatch()
-    const pokemones = useSelector(store => store.pokemones.array)
+    const pokemones = useSelector(store => store.pokemones.results)
     console.log(pokemones)
     return (
         <div>
             <h1>Lista de pokemones</h1>
             <button onClick={()=> dispatch(obtenerPokemonesAccion())}>Get Pokemones</button>
+            <button onClick={() => dispatch(siguientePokemonAction())}>Siguiente</button>
+            <button onClick={() => dispatch(anteriorPokemonAction())}>Anterior</button>
             <ul>
                 {
                     pokemones.map(item => (
--- src/Redux/pokeDucks.js
+++ src/Redux/pokeDucks.js
@@ -1,29 +1,64 @@
 import axios  from 'axios'
 // constantes
 const dataInicial = {
-    array : []
+    count:0,
+    next:null,
+    previus:null,
+    results:[]
 }
 
 // types
 const OBTENER_POKEMONES_EXITO = 'OBTENER_POKEMONES_EXITO'
+const SIGUIENTE_POKEMONES_EXITO = 'SIGUIENTE_POKEMONES_EXITO'
 // reducer
 export default function pokeReducer(state = dataInicial, action){
     switch(action.type){
         case OBTENER_POKEMONES_EXITO:
-            return {...state, array: action.payload}
+            return {...state, ...action.payload}
+        case SIGUIENTE_POKEMONES_EXITO:
+            return {...state, ...action.payload}    
             default:
                 return state
     }
 }
 // acciones
 export const obtenerPokemonesAccion = () =>async (dispatch, getState) => {
+    
 try {
-    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+    console.log(res.data)
     dispatch({
         type: OBTENER_POKEMONES_EXITO,
-        payload: res.data.results
+        payload: res.data
     })
 } catch (error) {
     console.log(error)
 }
+}
+
+export const siguientePokemonAction = (numero) => async(dispatch, getState) => {
+    const next = getState().pokemones.next
+    try {
+        const res = await axios.get(next)
+        dispatch({
+            type: SIGUIENTE_POKEMONES_EXITO,
+            payload: res.data
+        })
+    } catch (error) {
+        
+    }
+}
+
+export const anteriorPokemonAction = () => async(dispatch, getState) => {
+    const {previous} = getState().pokemones
+    console.log(previous)
+    try {
+        const res = await axios.get(previous)
+        dispatch({
+            type: SIGUIENTE_POKEMONES_EXITO,
+            payload: res.data
+        })
+    } catch (error) {
+        
+    }
 }
